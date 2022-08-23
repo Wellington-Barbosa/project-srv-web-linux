96b9c092b1a9493bef9ef0b170b990ea5bbdd291
Wellington Barbosa <wellington_ueg@hotmail.com>
2022-08-22 22:24:46 -0300

    Projeto de curso linux experience

--- script-iac2.sh
+++ script-iac2.sh
@@ -10,7 +10,7 @@ apt-get install unzip -y
 echo "Baixando e copiando os arquivos da aplicação..."
 
 cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+wget https://github.com/Wellington-Barbosa/site-linux-main.git
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html/
